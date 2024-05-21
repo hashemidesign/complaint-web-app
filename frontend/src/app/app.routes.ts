efe408a228f87849b42bd8e5c2d24ccb315087a9
Mohsen Hahsemi <m.hashemi.code@gmail.com>
2024-05-21 07:29:26 +0330

    add essential components and routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,41 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {LayoutComponent} from "./layout/layout.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {ComplaintCreateComponent} from "./complaint/complaint-create/complaint-create.component";
+import {ComplaintListComponent} from "./complaint/complaint-list/complaint-list.component";
+import {DepartmentComponent} from "./department/department.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+      },
+      {
+        path: 'complaint/create',
+        component: ComplaintCreateComponent,
+      },
+      {
+        path: 'complaint/list',
+        component: ComplaintListComponent,
+      },
+      {
+        path: 'department',
+        component: DepartmentComponent,
+      },
+    ]
+  }
+];
---
+++ frontend/src/app/complaint/complaint-create/complaint-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-complaint-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './complaint-create.component.html',
+  styleUrl: './complaint-create.component.scss'
+})
+export class ComplaintCreateComponent {
+
+}
