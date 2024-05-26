1d261bbe241a7657541d0bab0b6954c3c5a178d2
Mohsen Hahsemi <m.hashemi.code@gmail.com>
2024-05-26 08:29:10 +0330

    make layout

--- frontend/src/app/layout/layout.component.ts
+++ frontend/src/app/layout/layout.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {Router, RouterLink, RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterOutlet,
+    RouterLink
+  ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
 export class LayoutComponent {
+  private _router: Router = inject(Router);
+  loggedUserData: any;
 
+  constructor() {
+    const localData = localStorage.getItem('complaintUser');
+    if(localData != null) {
+      this.loggedUserData = JSON.parse(localData);
+    }
+  }
+
+  onLogOut() {
+    localStorage.removeItem('complaintUser');
+    this._router.navigateByUrl('/login');
+  }
 }
